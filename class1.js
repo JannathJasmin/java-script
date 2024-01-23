818cfddbd210450ddb994efeda3931b8e031c621
Jannath Jasmin <jannathjasmin01@gmail.com>
2024-01-23 13:03:24 +0530

    first commit

---
+++ class1.js
@@ -0,0 +1,4 @@
+document.getElementById("para").innerHTML="welcome to new class";
+document.getElementsByClassName("para1")[0].innerHTML="Welcome to JS class";
+document.getElementById("paraa").innerHTML="welcome";
+document.getElementsByClassName("parab")[0].innerHTML="hey";
